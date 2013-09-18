0e9f61554ae69c4ec964d9de88cbdcbbc05d82be
Callum <callum@callumj.co.uk>
2013-09-18 01:21:15 +0100

    Some refactoring to add authentication

--- server.py
+++ server.py
@@ -2,37 +2,51 @@
 
 import tornado.ioloop
 import tornado.web
-import sse
+import hmac
+import hashlib
+import functools
 import os
+import sse
 
 from tornado.options import define, options
 
-define('port', default=8888, help="run on the given port", type=int)
+define("port", default=8888, help="run on the given port", type=int)
 
-is_free = "yes"
+def hmac_authenticated(method):
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        hash = hmac.new(self.settings["hmac_key"],
+                        self.get_argument("data"))
+        if self.get_argument("token") != hash.hexdigest():
+            raise tornado.web.HTTPError(403, "Authentication required")
+        return method(self, *args, **kwargs)
+    return wrapper
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html",
+                    has_free_toilet=self.application.has_free_toilet)
+
+    @hmac_authenticated
+    def post(self):
+        free_toilet = self.application.has_free_toilet =\
+                      self.get_argument("data")
+        SSEHandler.write_message_to_all("message", free_toilet)
 
 class SSEHandler(sse.SSEHandler):
     pass
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        global is_free
-        self.render("index.html", is_free=is_free)
-
-    def post(self):
-        global is_free
-        is_free = self.get_argument("is_free", is_free)
-        SSEHandler.write_message_to_all("message", is_free)
-
-app = tornado.web.Application(
-    [
-        (r"/", MainHandler),
-        (r"/sse", SSEHandler),
-    ],
-    template_path=os.path.join(os.path.dirname(__file__), "templates")
-)
-
 if __name__ == "__main__":
+    base_path = os.path.dirname(__file__)
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+            (r"/sse", SSEHandler),
+        ],
+        template_path=os.path.join(base_path, "templates"),
+        hmac_key=open(os.path.join(base_path, ".hmac")).read().strip()
+    )
+    app.has_free_toilet = "yes"
     tornado.options.parse_command_line()
     app.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
