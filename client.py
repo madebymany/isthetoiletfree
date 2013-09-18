d0b4f8d96de903f04d556220d02fc5aee3f799fc
callum <callum@madebymany.co.uk>
2013-09-18 11:50:46 +0100

    Specify server as environment variable

--- client.py
+++ client.py
@@ -3,7 +3,6 @@
 import requests
 import hmac
 import hashlib
-import sys
 import os
 
 def call_api(state, **kwargs):
@@ -21,7 +20,7 @@ if __name__ == "__main__":
     import RPi.GPIO as io
     base_path = os.path.dirname(__file__)
 
-    API_URL = sys.argv[1]
+    API_URL = os.environ("ITTF_API_URL")
     HMAC_KEY = open(os.path.join(base_path, ".hmac_key")).read().strip()
     PINS = (8, 16, 18)
 
