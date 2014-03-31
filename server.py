74fe0936cb061f2f20b728c6f41b70583ec037ef
Callum <callum@madebymany.co.uk>
2014-03-31 18:31:50 +0100

    Add simple stats page with Google Auth

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import tornado.ioloop
 import tornado.web
 import tornado.gen
 import tornado.websocket
+import tornado.auth
 import tornado.escape
 import hmac
 import hashlib
@@ -11,6 +12,10 @@ import functools
 import os
 import momoko
 import urlparse
+import time
+import datetime
+import parsedatetime
+import prettytable
 import logging
 
 from tornado.options import define, options
@@ -21,6 +26,16 @@ define("db_port", default=5432, help="database port", type=int)
 define("db_name", default="callum", help="database name", type=str)
 define("db_user", default="callum", help="database username", type=str)
 define("db_pass", default="", help="database password", type=str)
+
+
+class DateStrParser(object):
+    def __init__(self):
+        self.calendar = parsedatetime.Calendar()
+
+    def parse(self, str):
+        return datetime.datetime.fromtimestamp(
+            time.mktime(self.calendar.parse(str)[0]))
+
 
 def get_psql_credentials():
     try:
@@ -80,10 +95,27 @@ class BaseHandler(tornado.web.RequestHandler):
     def db(self):
         return self.application.db
 
+    def get_current_user(self):
+        return self.get_secure_cookie("ittf_user")
+
     @tornado.gen.coroutine
     def has_free_toilet(self):
         cursor = yield momoko.Op(self.db.callproc, "has_free_toilet")
         raise tornado.gen.Return(cursor.fetchone()[0])
+
+
+class GoogleLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
+    @tornado.gen.coroutine
+    def get(self):
+        if self.get_argument("openid.mode", None):
+            email = (yield self.get_authenticated_user())["email"]
+            if email.endswith("@madebymany.co.uk") or \
+               email.endswith("@madebymany.com"):
+                self.set_secure_cookie("ittf_user", email)
+                self.redirect("/stats")
+            self.redirect("/")
+        else:
+            yield self.authenticate_redirect()
 
 
 class MainHandler(BaseHandler):
@@ -116,6 +148,68 @@ class MainHandler(BaseHandler):
                 logging.error("Error sending message", exc_info=True)
 
 
+class StatsHandler(BaseHandler):
+    @tornado.web.authenticated
+    @tornado.gen.coroutine
+    def get(self):
+        date_range = self._get_date_range()
+        op = date_range["op"]
+        clause = None
+        if op:
+            clause = yield momoko.Op(self.db.mogrify, *op)
+        queries = [
+            ("Number of visits",
+             "SELECT toilet_id, count(*) "
+             "AS num_visits FROM visits %(clause)s "
+             "GROUP BY toilet_id ORDER BY toilet_id;"),
+            ("Average visit duration",
+             "SELECT toilet_id, avg(duration) "
+             "AS duration_avg FROM visits %(clause)s "
+             "GROUP BY toilet_id ORDER BY toilet_id;"),
+            ("Minimum visit duration",
+             "SELECT toilet_id, min(duration) "
+             "AS duration_min FROM visits %(clause)s "
+             "GROUP BY toilet_id ORDER BY toilet_id;"),
+            ("Maximum visit duration",
+             "SELECT toilet_id, max(duration) "
+             "AS duration_max FROM visits %(clause)s "
+             "GROUP BY toilet_id ORDER BY toilet_id;")
+        ]
+        results = yield [momoko.Op(self.db.execute,
+                                   q % {"clause": clause or ""}) \
+                         for _, q in queries]
+        self.render("stats.html", date_range=date_range,
+                    tables=[(queries[i][0], prettytable.from_db_cursor(r)) \
+                            for i, r in enumerate(results)])
+
+    def _get_date_range(self):
+        parser = DateStrParser()
+        parsed_start = None
+        parsed_end = None
+        text = None
+        op = None
+        start = self.get_argument("from", None)
+        end = self.get_argument("to", None)
+
+        if start and end:
+            parsed_start = parser.parse(start)
+            parsed_end = parser.parse(end)
+            text = "Showing from %s til %s" % (parsed_start, parsed_end)
+            op = ("WHERE recorded_at BETWEEN %s AND %s",
+                  (parsed_start, parsed_end))
+        elif start:
+            parsed_start = parser.parse(start)
+            text = "Showing from %s onward" % parsed_start
+            op = ("WHERE recorded_at >= %s", (parsed_start,))
+        elif end:
+            parsed_end = parser.parse(end)
+            text = "Showing from %s backward" % parsed_end
+            op = ("WHERE recorded_at <= %s", (parsed_end,))
+
+        return dict(op=op, start=start, end=end, text=text,
+                    parsed_start=parsed_start, parsed_end=parsed_end)
+
+
 class APIHandler(BaseHandler):
     @tornado.gen.coroutine
     def get(self):
@@ -131,11 +225,15 @@ class APIHandler(BaseHandler):
 
 if __name__ == "__main__":
     app = tornado.web.Application(
-        [(r"/", MainHandler),
+        [(r"/login", GoogleLoginHandler),
+         (r"/", MainHandler),
+         (r"/stats", StatsHandler),
          (r"/api", APIHandler),
          (r"/hasfreesocket", HasFreeWebSocketHandler)],
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        hmac_key=get_secret_key()
+        hmac_key=get_secret_key(),
+        cookie_secret="CGT6NALTvnR8LpILJwyKGcwFzntjna7w",
+        login_url="/login"
     )
     app.db = momoko.Pool(
         dsn=" ".join(["%s=%s" % c for c in get_psql_credentials().iteritems()]),
